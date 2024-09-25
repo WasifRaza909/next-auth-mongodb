c3c9706fd7c88dab57db5cda7c53aa1ef825ebd6
wasif-technyx <142510796+wasif-technyx@users.noreply.github.com>
2024-09-25 11:39:47 +0500

    Integrated DB WIP

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 // We have to connect db in every file when adding apis in nextjs, in node we have to connect only once
 import { connect } from "@/dbConfig/dbConfig";
 import User from "@/models/userModel";
@@ -6,5 +8,22 @@ import { NextRequest, NextResponse } from "next/server";
 connect();
 
 export async function POST(request: NextRequest) {
-  const { username, email, password } = request.body;
+  try {
+    const reqBody = request.json();
+    const { username, email, password } = reqBody;
+
+    // validation
+    console.log(reqBody);
+
+    const user = await User.findOne({ email });
+
+    if (user) {
+      return NextResponse.json(
+        { error: "User already exists" },
+        { status: 400 }
+      );
+    }
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
 }
