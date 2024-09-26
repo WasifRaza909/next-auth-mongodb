ff3f1d7ec2458f037c75469a84ba38619a5ee84c
wasif-technyx <142510796+wasif-technyx@users.noreply.github.com>
2024-09-26 11:15:02 +0500

    WIP

--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -1,18 +1,32 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
+import User from "@/models/userModel";
 import nodemailer from "nodemailer";
+import bcryptjs from "bcryptjs";
 
 export const sendEmail = async ({ email, emailType, userId }: any) => {
   try {
-    // TODO: configure mail for usage
+    const hashedToken = bcryptjs.hash(userId.toString(), 10);
 
-    const transporter = nodemailer.createTransport({
-      host: "smtp.ethereal.email",
-      port: 587,
-      secure: false,
+    if (emailType === "VERIFY") {
+      await User.findByIdAndUpdate(userId, {
+        verifyToken: hashedToken,
+        verifyTokenExpiry: Date.now() + 3600000,
+      });
+    } else if (emailType === "RESET") {
+      await User.findByIdAndUpdate(userId, {
+        forgotPasswordToken: hashedToken,
+        forgotPasswordTokenExpiry: Date.now() + 3600000,
+      });
+    }
+
+    // Looking to send emails in production? Check out our Email API/SMTP product!
+    const transport = nodemailer.createTransport({
+      host: "sandbox.smtp.mailtrap.io",
+      port: 2525,
       auth: {
-        user: "maddison53@ethereal.email",
-        pass: "jn7jnAPss4f63QBp6D",
+        user: "6549ac5070b485", // (It should be in .env),
+        pass: "542eeb497d1de4", // (It should be in .env),
       },
     });
 
@@ -21,7 +35,13 @@ export const sendEmail = async ({ email, emailType, userId }: any) => {
       to: email,
       subject:
         emailType === "VERIFY" ? "Verify your email" : "Reset your password",
-      html: "<b>Hello world?</b>",
+      html: `<p>Click <a href="${
+        process.env.DOMAIN
+      }/verifyemail?token=${hashedToken}">here</a> to ${
+        emailType === "VERIFY" ? "verify your email" : "reset your password"
+      } or copy and paste the link below in your browser.<br> ${
+        process.env.DOMAIN
+      }/verifyemail?token=${hashedToken}</p>`,
     };
 
     const mailResponse = await transporter.sendMail(mailOptions);
