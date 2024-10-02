2eee3b73237df575a008d938560918e8ae363020
wasif-technyx <142510796+wasif-technyx@users.noreply.github.com>
2024-10-02 11:16:15 +0500

    frontend pages created, installed react-hot-toast & axios

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
+/* eslint-disable @typescript-eslint/no-explicit-any */
 
-const page = () => {
-  return (
-    <div>page</div>
-  )
-}
+import React, { useState } from "react";
 
-export default page
+import axios from "axios";
+import { toast } from "react-hot-toast";
+import { useRouter } from "next/navigation";
+
+const SignupPage = () => {
+  const router = useRouter()
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+    username: "",
+  });
+
+  const [buttonDisabled, setButtonDisabled] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const onSignup = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.post("/api/users/signup", user);
+
+      console.log("Signup success", response.data);
+      router.push('/login')
+    } catch (error: any) {
+      console.log("Signup failed");
+      toast.error(error.message)
+    }
+  };
+
+  return <div>Signup page</div>;
+};
+
+export default SignupPage;
